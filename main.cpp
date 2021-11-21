6c827881d86890962bdb719304001ae7895a3f0d
JDVB07 <92609924+JDVB07@users.noreply.github.com>
2021-11-21 12:23:18 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+	int i;
+	
+	for(i=1; i<=70; i++){
+	cout <<i<<endl;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+getch();	
+return 0;
+}
